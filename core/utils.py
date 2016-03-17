f7988f9e431bc027d7f1bb4c800c27eb81327864
Marcos Santamaria <marcosc74@gmail.com>
2016-03-17 21:12:36 +0100

    ak_sync.py
    Revision of ak_sync. Pending to refactor

--- core/utils.py
+++ core/utils.py
@@ -3,21 +3,22 @@
 #
 #
 
-def log( msg ):
-   print( msg )
+def log( msg, verbose_mode = True ):
+   if verbose_mode:
+      print( msg )
 
 
-def log_item( item, is_commented=False ):
+def log_item( item, is_commented=False, verbose = True ):
    if is_commented:
-      log( "  - add COMMENTED item: " + item )
+      log( "  - add COMMENTED item: " + item, verbose )
    else:
-      log( "  - add item: " + item )
+      log( "  - add item: " + item, verbose )
 
 
-def log_album( album ):
-   log( "- add album: " + album )
+def log_album( album, verbose = True ):
+   log( "- add album: " + album, verbose )
 
 
-def log_error( msg ):
-   log( "*** " + msg )
+def log_error( msg, verbose = True ):
+   log( "*** " + msg, verbose )
 
